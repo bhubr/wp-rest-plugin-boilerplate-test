a8bf01aed294b8134a30b72a27486d37c2ebd05c
Benoît Hubert <benoithubert@gmail.com>
2016-09-13 15:06:42 +0200

    Perform creation of two objects at once

--- js/tests.js
+++ js/tests.js
@@ -1,6 +1,6 @@
 var $ = jQuery;
 
-var sendRequest = function(relativeUrl, method, payload) {
+function sendRequest(relativeUrl, method, payload) {
   return new Promise(function(resolve, reject) {
     var baseUrl = '/wp-json/bhubr/v1/foos';
     $.ajax({
@@ -10,13 +10,19 @@ var sendRequest = function(relativeUrl, method, payload) {
       dataType: 'json',
       headers: { "Content-Type": 'application/json' },
       success: function(data) {
-        console.log(data);
         resolve(data);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         reject(errorThrown)
       } });
   });
+}
+
+function getTimestamp() {
+  return (new Date().getTime() + Math.ceil(1000000 * Math.random())).toString(36);
+}
+function getPayload(timestamp) {
+  return JSON.stringify({ name: 'blah@blah.com ' + timestamp, description: 'Verbose blah blah', foo_type: 'Foo', foo_number: 5, ignored_field: 'ignored' });
 }
 
 var requests = {
@@ -26,8 +32,8 @@ var requests = {
     return sendRequest('', 'POST', payload);
   },
 
-  delete: function(id, payload) {
-    console.log('deleteObject', payload);
+  delete: function(id) {
+    console.log('deleteObject');
     return sendRequest('/' + id, 'DELETE');
   }
 
@@ -35,11 +41,10 @@ var requests = {
 
 $(document).ready(function() {
 
-  QUnit.test( "hello test POST", function( assert ) {
+  QUnit.test( "Test Object (of type Post) creation/read/delete by POST request", function( assert ) {
     var done = assert.async();
-    var timestamp = (new Date().getTime()).toString(36);
-    var payload = JSON.stringify({ name: 'blah@blah.com ' + timestamp, description: 'Verbose blah blah', foo_type: 'Foo', foo_number: 5, ignored_field: 'ignored' });
-    // $.ajax({ method: 'POST', url: '/wp-json/bhubr/v1/foos', data: payload, success: function(data) {
+    var timestamp = getTimestamp();
+    var payload = getPayload(timestamp);
     requests.create(payload).then(function(data) {
       var keys = Object.keys(data);
       assert.equal(data.name, 'blah@blah.com ' + timestamp);
@@ -50,7 +55,42 @@ $(document).ready(function() {
       assert.equal(data.ignored_field, 'ignored');
       assert.equal(keys.length, 7);
       assert.deepEqual(keys, ['id', 'slug', 'name', 'description', 'foo_type', 'foo_number', 'ignored_field']);
-      requests.delete(data.id).then(done);
+      requests.delete(data.id).then(function(result) {
+        assert.ok(result.success);
+        done();
+      });
+    });
+  });
+
+  QUnit.test( "Test Object (of type Post) creation, read all, delete", function( assert ) {
+    var done = assert.async();
+    var timestamp1 = getTimestamp();
+    var timestamp2 = getTimestamp();
+    var id1;
+    var id2;
+    console.log(timestamp1, timestamp2);
+    Promise.all([
+      requests.create(getPayload(timestamp1)),
+      requests.create(getPayload(timestamp2))
+    ])
+    .then(function(datas) {
+      assert.equal(datas.length, 2);
+      id1 = datas[0].id;
+      id2 = datas[1].id;
+    })
+    .then(function() {
+      return;
+    })
+    .then(function() {
+      return Promise.all([
+        requests.delete(id1),
+        requests.delete(id2)
+      ]);
+    })
+    .then(function(results) {
+      assert.ok(results[0].success);
+      assert.ok(results[1].success);
+      done();
     });
   });
 
